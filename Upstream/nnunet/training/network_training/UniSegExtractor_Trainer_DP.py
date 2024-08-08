e533576fb52adc360892b110ccd6dff414e89fc7
Marcell Borhi <mborhi@iu.edu>
2024-08-08 11:01:29 +0000

    4 gpu train

--- Upstream/nnunet/training/network_training/UniSegExtractor_Trainer_DP.py
+++ Upstream/nnunet/training/network_training/UniSegExtractor_Trainer_DP.py
@@ -84,7 +84,7 @@ class UniSegExtractor_Trainer_DP(nnUNetTrainerV2_DP):
         print("task_class", self.task_class)
         self.visual_epoch = -1
         self.total_task_num = 11 # NOTE
-        self.num_batches_per_epoch = int((50 // 4) * self.total_task_num)
+        self.num_batches_per_epoch = int((50 // 2) * self.total_task_num)
         print("num batches per epoch:", self.num_batches_per_epoch)
         print("total task num", self.total_task_num)
         print(os.getcwd())
